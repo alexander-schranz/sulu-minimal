1aa480cd3a82276c3ff1c647f4aeaa3d34b9f215
Alexander Schranz <alexander@sulu.io>
2019-02-21 00:21:34 +0100

    Temp

---
+++ assets/admin/index.js
@@ -0,0 +1,18 @@
+// Bundles
+import 'sulu-admin-bundle';
+import 'sulu-contact-bundle';
+import 'sulu-media-bundle';
+import 'sulu-page-bundle';
+import 'sulu-preview-bundle';
+import 'sulu-security-bundle';
+
+// Polyfills
+import 'core-js/fn/array/includes';
+import 'core-js/fn/array/find-index';
+import 'core-js/fn/array/fill';
+import 'core-js/fn/array/from';
+import 'core-js/fn/promise';
+import 'core-js/fn/symbol';
+import 'whatwg-fetch';
+import 'url-search-params-polyfill';
+import 'regenerator-runtime/runtime';
